cac0cacb2bd0c3d787241d70f1b9ddea2e983639
Kim-Kristian Seppälä <104766738+lazylord-portfolio@users.noreply.github.com>
2022-05-17 00:58:48 +0300

    visual touches and animation
    Doing visual touch-up on the doc and adding a fade in-and-out animation to the footer in JS. Learning a new way of making use of observers.

--- js/fadein.js
+++ js/fadein.js
@@ -1,7 +1,7 @@
 
 
 
-const firstobs = new IntersectionObserver(entries => {
+const first_obs_in = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             document.querySelectorAll(".first-section")[0].classList.add("fadein-left");
@@ -9,9 +9,7 @@ const firstobs = new IntersectionObserver(entries => {
     })
 });
 
-firstobs.observe(document.querySelector(".first-section"));
-
-const secondobs = new IntersectionObserver(entries => {
+const second_obs_in = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             document.querySelectorAll(".second-section")[0].classList.add("fadein-right");
@@ -19,14 +17,68 @@ const secondobs = new IntersectionObserver(entries => {
     })
 });
 
-secondobs.observe(document.querySelector(".second-section"));
-
-const thirdobs = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            document.querySelectorAll(".third-section")[0].classList.add("fadein-left");
-        }
-    })
+const third_obs_in = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+        document.querySelectorAll(".third-section")[0].classList.add("fadein-left");
+    }
 });
 
-thirdobs.observe(document.querySelector(".third-section"));
+
+first_obs_in.observe(document.querySelector(".first-section"));
+
+second_obs_in.observe(document.querySelector(".second-section"));
+
+third_obs_in.observe(document.querySelector(".third-section"));
+
+
+
+/*Adding a different type of transition*/
+const numSteps = 20.0;
+
+let header;
+let prevRatio = 0.0;
+let opacity = 0;
+
+window.addEventListener("load", (event) => {
+    header = document.querySelector("#fader");
+
+    create_observer();
+}, false)
+
+
+function create_observer() {
+    let options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: build_thresholds()
+    }
+
+    const obs = new IntersectionObserver(fadeout, options);
+    obs.observe(header);
+}
+
+
+function build_thresholds() {
+    let thresholds = [];
+    let steps = 20;
+
+    for (let i = 1.0; i <= steps; i++)
+        thresholds.push(i / steps);
+
+    thresholds.push(0);
+    return thresholds;
+}
+
+function fadeout(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > prevRatio) {
+            entry.target.style.opacity = entry.intersectionRatio;
+        } else {
+            entry.target.style.opacity = entry.intersectionRatio;
+        }
+
+        prevRatio = entry.intersectionRatio;
+    })
+}
+
+
